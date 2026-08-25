f35ee371570da44f62d300d0104585d168ab1da7
Marcos Cardoso <marcosvinimf@gmail.com>
2026-08-25 14:50:44 -0300

    First try exercise 11

--- Listas/lista1.11.c
+++ Listas/lista1.11.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* concatenar (int* v1, int v1Size, int* v2, int v2Size, int *n3){
+    *n3 = v1Size + v2Size;
+    int* v3 = (int*)malloc(sizeof(int) * *n3);
+    for (int i = 0; i < *n3; i++){
+        if (i < v1Size) v3[i] = v1[i];
+        if (i < v1Size + v2Size) v3[i] = v2[i];
+    }
+}
+
 int* montar_vetor (int* tamanho){
     int *v = NULL;
     int count = 0;
@@ -33,17 +42,17 @@ int* montar_vetor (int* tamanho){
 
 
 int main(){
-    int* v = NULL;
-    int* addnumbers = NULL;
-
     int tamanho1 = 0;
     int tamanho2 = 0;
     int* v1 = montar_vetor(&tamanho1);
     int* v2 = montar_vetor(&tamanho2);
 
-    
-    
-    
+    int tamanho3;
+
+    int* v3 = concatenar(v1, tamanho1, v2, tamanho2, &tamanho3);
+    for (int i = 0; i < tamanho3; i++){
+        printf("%d", v3[i]);
+    }
     
     /*int v2Size = count;
     int* v3Size = NULL; 
