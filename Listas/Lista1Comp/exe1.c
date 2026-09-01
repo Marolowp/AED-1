c27840aee3cead826ada44e69a069b23ba13f77c
Marcos Cardoso <marcosvinimf@gmail.com>
2026-09-01 14:25:19 -0300

    Complementary exercises solutions

--- Listas/Lista1Comp/exe1.c
+++ Listas/Lista1Comp/exe1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
     int x;
@@ -6,14 +7,29 @@ typedef struct {
 } Ponto;
 
 Ponto *criar_ponto (int x, int y){
+    Ponto *p = (Ponto*)malloc(sizeof(Ponto));
+    if (p == NULL){
+        printf("Erro ao alocar memoria.");
+        free(p);
+        exit(1);
+    }
 
+    p->x = NULL;
+    p->y = NULL;
+    return p;
 }
 
 void destruir_ponto(Ponto *p){
-
+    free(p);
 }
 
 int main(){
+    Ponto* p1 = criar_ponto(20,10);
     
+    if (p1 != NULL){
+        printf("Ponto criado: (%d, %d)", p1->x, p1->y);
+        destruir_ponto(p1);
+    }
+
 
 }
